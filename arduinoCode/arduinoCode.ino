2b0652447b5c2e946972f114f5aeb764dc100bc2
Adrian <adrianrosten@gmail.com>
2024-01-23 19:38:45 +0100

    Changed the arudino code to store name in EEPROM

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
 const int MAX_INPUT_LENGTH = 64; // Maximum length for input command
 const int PWM_PINS[] = {3, 5, 6, 9, 10, 11}; // PWM-capable pins on Arduino Nano
 const int NUM_PWM_PINS = sizeof(PWM_PINS) / sizeof(PWM_PINS[0]);
-const String whodis = "ch1";
+//const String whodis = "ch1";
+const int EEPROM_START_ADDR = 0; // Starting address for EEPROM string storage
+
+// Function to write a string to EEPROM starting at a specified address
+void eepromWriteString(int addr, const char* string) {
+  int i = 0;
+  while (string[i] != '\0') {
+    EEPROM.write(addr + i, string[i]);
+    i++;
+  }
+  EEPROM.write(addr + i, '\0'); // Write the null terminator
+}
+
+// Function to read a string from EEPROM starting at a specified address
+String eepromReadString(int addr) {
+  String string;
+  char ch = EEPROM.read(addr);
+  while (ch != '\0' && addr < EEPROM.length()) {
+    string += ch; // Append character to string
+    addr++;
+    ch = EEPROM.read(addr);
+  }
+  return string;
+}
 
 bool isPWMPin(int pin) {
   for (int i = 0; i < NUM_PWM_PINS; i++) {
@@ -53,7 +77,12 @@ String doStuff(String line) {
           returned += "o;";
           break;
         case 'w':
-          returned += whodis + ";";
+          returned += eepromReadString(EEPROM_START_ADDR) + ";";
+          break;
+        case 'e': // write to eeprom
+          // Write the remaining part of the command to EEPROM
+          eepromWriteString(EEPROM_START_ADDR, args.c_str());
+          returned += eepromReadString(EEPROM_START_ADDR) + ";";
           break;
         default:
           returned += "Error: Unknown command;";
@@ -67,6 +96,13 @@ String doStuff(String line) {
 
 void setup() {
   Serial.begin(9600);
+
+  String readd = eepromReadString(EEPROM_START_ADDR);
+  if (readd == "" or readd.charAt(0) == -1) {
+    // If empty, write "unnamed device" to EEPROM
+
+    eepromWriteString(EEPROM_START_ADDR, "unnamed device");
+  }
 }
 
 void loop() {
